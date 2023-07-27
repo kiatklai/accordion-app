567cdc72f87fc542bf4fb0e2ec30a903730b8881
kiatklai <likhitthaworn.k@gmail.com>
2023-07-27 16:02:18 +0900

    ボタンをアイコンにする

--- src/components/SingleContent.js
+++ src/components/SingleContent.js
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { AiOutlinePlus,AiOutlineMinus } from "react-icons/ai";
+
 
 const SingleContent=({title,description})=>{
   const [showContent,setShowContent] = useState(false)
@@ -6,7 +8,9 @@ const SingleContent=({title,description})=>{
     <article className="content">
       <header>
         <h4>{title}</h4>
-        <button className="btn" onClick={()=>setShowContent(!showContent)}>More Info</button>
+        <button className="btn" onClick={()=>setShowContent(!showContent)}>
+          {showContent ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+        </button>
       </header>
       {showContent && <p>{description}</p>}
     </article>
